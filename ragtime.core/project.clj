e512182313f7f760bdddbae99ff42db4b0b7563c
Michael Klishin <michael@clojurewerkz.org>
2014-10-25 17:52:22 +0400

    Depend on Clojure 1.6.0

--- ragtime.core/project.clj
+++ ragtime.core/project.clj
@@ -1,6 +1,6 @@
 (defproject clojurewerkz/ragtime.core "0.3.9"
   :description "A database-independent migration library"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.2.2"]]
   :aot [ragtime.main]
   :clean-non-project-classes true

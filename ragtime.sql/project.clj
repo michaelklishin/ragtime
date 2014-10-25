f4643ea35481e6eeb2540108b4318f87f54bc58f
Michael Klishin <michael@clojurewerkz.org>
2014-10-25 17:57:05 +0400

    0.4.0

--- ragtime.sql/project.clj
+++ ragtime.sql/project.clj
@@ -1,7 +1,7 @@
-(defproject clojurewerkz/ragtime.sql "0.3.9"
+(defproject clojurewerkz/ragtime.sql "0.4.0"
   :description "Ragtime migrations for SQL databases"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clojurewerkz/ragtime.core "0.3.9"]
+                 [clojurewerkz/ragtime.core "0.4.0"]
                  [org.clojure/java.jdbc "0.2.3"]]
   :profiles
   {:dev {:dependencies [[com.h2database/h2 "1.3.160"]]}

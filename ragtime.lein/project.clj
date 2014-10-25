ee069d03ba2728e3db2ebde64e8011996dc8416d
Michael Klishin <michael@clojurewerkz.org>
2014-10-25 17:54:04 +0400

    0.4.0

--- ragtime.lein/project.clj
+++ ragtime.lein/project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/ragtime.lein "0.3.9"
+(defproject clojurewerkz/ragtime.lein "0.4.0"
   :description "A leiningen plugin for Ragtime."
   :dependencies [[leinjacker "0.4.1"]]
   :eval-in-leiningen true)
